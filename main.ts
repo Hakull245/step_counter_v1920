5ef73b414ddd0eaf51d8562f725492292d22110a
Hakull245 <hoangha2452002@gmail.com>
2021-11-30 16:21:22 +0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,23 +1,33 @@
 input.onButtonPressed(Button.A, function () {
-	
+    basic.showNumber(steps)
+    basic.showString(" steps, ")
+    basic.showNumber(distance / 100)
+    basic.showString(" m")
 })
 input.onButtonPressed(Button.AB, function () {
     steps = 0
+    start_time = input.runningTime()
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showNumber(distance)
+    finish_time = input.runningTime()
+    time = (finish_time - start_time) / 1000
+    basic.showNumber(time)
+    basic.showString("s ,")
+    basic.showNumber(time * 15)
+    basic.showString("cal")
 })
-input.onGesture(Gesture.Shake, function () {
-	
-})
+let time = 0
+let finish_time = 0
 let distance = 0
+let start_time = 0
 let steps = 0
 steps = 0
 basic.showNumber(0)
+start_time = input.runningTime()
 basic.forever(function () {
     if (input.acceleration(Dimension.Strength) > 1500) {
         steps += 1
-        distance += steps * 16
+        distance += steps * 26
         basic.showNumber(steps)
     }
 })
